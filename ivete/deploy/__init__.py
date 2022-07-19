ca006096bd65fe624a2b6d423ad94f3de539f147
Seongbum Seo <seo@seongbum.com>
2022-07-19 14:44:31 +0900

    Update deploy method

--- ivete/deploy/__init__.py
+++ ivete/deploy/__init__.py
@@ -1,21 +1,21 @@
 from .arguments import DeployArguments
 
-def deploy(inference):
+def deploy(inference, template_name):
     from flask import Flask, request, jsonify, render_template
     from flask_cors import CORS
 
-    app = Flask(__name__, template_folder='templates')
+    app = Flask(__name__, template_folder=f"templates/{template_name}")
     CORS(app)
 
-    @app.route('/')
+    @app.route("/")
     def index():
-        return render_template('index.html')
+        return render_template("index.html")
 
-    @app.route('/api', methods=['POST'])
+    @app.route("/api", methods=["POST"])
     def api():
-        query = request.json
-        #data = inference(...)
-        #response = jsonify(data)
+        query = request.json['query']
+        data = inference(query)
+        response = jsonify(data)
         return response
 
     return app
