f5d9ec9753a2548f230d078d6568d3c087818b33
Seongbum Seo <seo@seongbum.com>
2022-07-20 10:18:12 +0900

    Update deploy method
    - Apply url prefix to api name
    - Change parameters to be inputed as spreaded arguments instead of a packed dict

--- ivete/deploy/__init__.py
+++ ivete/deploy/__init__.py
@@ -1,21 +1,31 @@
 from .arguments import DeployArguments
 
-def deploy(inference, template_name):
-    from flask import Flask, request, jsonify, render_template
+def deploy(inference, api_name, template_name, template_path=None):
+    import os
+    from flask import Flask, Blueprint, request, jsonify, render_template
     from flask_cors import CORS
+    from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
-    app = Flask(__name__, template_folder=f"templates/{template_name}")
-    CORS(app)
-
-    @app.route("/")
+    template_folder = os.path.join(template_path, template_name)
+    bp = Blueprint(
+        api_name,
+        __name__,
+        template_folder=template_folder,
+    )
+    
+    @bp.route("/")
     def index():
         return render_template("index.html")
 
-    @app.route("/api", methods=["POST"])
+    @bp.route("/api", methods=["POST"])
     def api():
-        query = request.json['query']
-        data = inference(query)
+        input = request.json
+        data = inference(**input)
         response = jsonify(data)
         return response
+
+    app = Flask(__name__)
+    app.register_blueprint(bp, url_prefix=f"/{api_name}")
+    CORS(app)
 
     return app
